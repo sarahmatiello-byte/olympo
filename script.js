fde24eb765204e72e78bb530a7ca678f3ea9b454
sarahmatiello-byte <sarah.matiello@escola.pr.gov.br>
2026-08-17 22:18:52 -0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let curtidas = 0;
+let joinhas = 0;
 
+function curtir() {
+    curtidas++;
+    document.getElementById("contadorCurtidas").textContent = curtidas;
+}
+
+function joinha() {
+    joinhas++;
+    document.getElementById("contadorJoinhas").textContent = joinhas;
+}
