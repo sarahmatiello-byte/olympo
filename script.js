02a7a9b317de6cf38c6f2d496b9727bf6aed86b9
sarahmatiello-byte <sarah.matiello@escola.pr.gov.br>
2026-09-03 22:38:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ function joinha() {
     joinhas++;
     document.getElementById("contadorJoinhas").textContent = joinhas;
 }
+const temaBtn = document.getElementById("temaBtn");
+
+temaBtn.addEventListener("click", function() {
+    document.body.classList.toggle("tema-escuro");
+
+    if (document.body.classList.contains("tema-escuro")) {
+        temaBtn.textContent = "☀️";
+    } else {
+        temaBtn.textContent = "🌙";
+    }
+});
